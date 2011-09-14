3a9d324d67c57ba19a78ee609f56fb84adbe8037
Josua Krause <josua.krause@googlemail.com>
2011-09-14 13:35:48 +0200

    factory method

--- src/jkit/pen/AbstractShapeDrawer.java
+++ src/jkit/pen/AbstractShapeDrawer.java
@@ -7,4 +7,8 @@ public abstract class AbstractShapeDrawer {
 
 	public abstract void draw(final Graphics gfx, final Shape outline);
 
+	public static final AbstractShapeDrawer getShapeDrawerForPen(final Pen p) {
+		return p != null ? new PenShapeDrawer(p) : new SimpleShapeDrawer();
+	}
+
 }
