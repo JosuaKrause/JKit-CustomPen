d0d7ec698728c03663baf1e51cff8bb52e13400a
Josua Krause <josua.krause@googlemail.com>
2011-09-20 15:20:16 +0200

    more flexible abstract shape drawer

--- src/jkit/gfx/SimpleShapeDrawer.java
+++ src/jkit/gfx/SimpleShapeDrawer.java
@@ -2,13 +2,36 @@ package jkit.gfx;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Paint;
 import java.awt.Shape;
+import java.awt.Stroke;
 
 public class SimpleShapeDrawer extends AbstractShapeDrawer {
 
+	private final Stroke stroke;
+
+	private final Paint paint;
+
+	public SimpleShapeDrawer() {
+		this(null, null);
+	}
+
+	public SimpleShapeDrawer(final Stroke stroke, final Paint paint) {
+		this.stroke = stroke;
+		this.paint = paint;
+	}
+
 	@Override
 	public void draw(final Graphics gfx, final Shape outline) {
-		((Graphics2D) gfx).draw(outline);
+		final Graphics2D g = (Graphics2D) gfx.create();
+		if (stroke != null) {
+			g.setStroke(stroke);
+		}
+		if (paint != null) {
+			g.setPaint(paint);
+		}
+		g.draw(outline);
+		g.dispose();
 	}
 
 }
