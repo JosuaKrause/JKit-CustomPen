876e1da1cf226921f7ced5206e366c862b5aa2fd
Joschi <josua.krause@googlemail.com>
2011-10-08 11:56:02 +0200

    better testing app

--- src/test/Test.java
+++ src/test/Test.java
@@ -5,13 +5,17 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Shape;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.geom.GeneralPath;
 
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.WindowConstants;
 
 import jkit.gfx.AbstractShapeDrawer;
 import jkit.gfx.PenShapeDrawer;
+import jkit.gfx.SimpleShapeDrawer;
 import jkit.gfx.pen.ArrowPen;
 import jkit.gfx.pen.BloodTrailPen;
 import jkit.gfx.pen.CirclePen;
@@ -21,69 +25,85 @@ import jkit.gfx.pen.PencilPen;
 
 public class Test {
 
-	private static JFrame jFrame;
-	private static Shape shape;
-	private static AbstractShapeDrawer shapeDrawer;
+    private static JFrame jFrame;
+    private static Shape shape;
+    private static AbstractShapeDrawer shapeDrawer;
+    private static int curPen = 0;
+    private static Pen[] pens = new Pen[] {
 
-	public static void main(final String[] args) {
-		shape = createShape();
-		Pen p;
-		final int a = 3;
-		switch (a) {
-		case 0:
-			p = new FacingTrianglePen(Color.BLUE, 30.0);
-			break;
-		case 1:
-			p = new BloodTrailPen();
-			break;
-		case 2:
-			p = new PencilPen();
-			break;
-		case 3:
-			p = new ArrowPen();
-			break;
-		default:
-			p = new CirclePen();
-			break;
-		}
-		shapeDrawer = new PenShapeDrawer(p);
-		jFrame = new JFrame();
-		jFrame.add(new JComponent() {
+    null,
 
-			private static final long serialVersionUID = 2257038010168418793L;
+    new PencilPen(),
 
-			@Override
-			protected void paintComponent(final Graphics gfx) {
-				final Graphics2D g = (Graphics2D) gfx.create();
-				// g.setColor(Color.BLACK);
-				// g.draw(shape);
-				shapeDrawer.draw(g, shape);
-				g.dispose();
-			}
+    new BloodTrailPen(),
 
-		});
-		jFrame.setSize(new Dimension(800, 600));
-		jFrame.setLocationRelativeTo(null);
-		jFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		jFrame.setVisible(true);
-	}
+    new ArrowPen(),
 
-	private static Shape createShape() {
-		final GeneralPath path = new GeneralPath();
-		path.moveTo(100.0, 100.0);
-		path.lineTo(150.0, 200.0);
-		path.quadTo(250.0, 250.0, 300.0, 150.0);
-		path.curveTo(400.0, 80.0, 450.0, 160.0, 500.0, 400.0);
-		path.moveTo(600.0, 300.0);
-		path.quadTo(650.0, 250.0, 700.0, 300.0);
-		path.quadTo(700.0, 350.0, 650.0, 350.0);
-		path.closePath();
-		path.moveTo(200.0, 350.0);
-		path.quadTo(200.0, 300.0, 250.0, 300.0);
-		path.quadTo(300.0, 300.0, 300.0, 350.0);
-		path.quadTo(300.0, 400.0, 250.0, 400.0);
-		path.quadTo(200.0, 400.0, 200.0, 350.0);
-		return path;
-	}
+    new FacingTrianglePen(Color.BLUE, 30.0),
+
+    new CirclePen(),
+
+    };
+
+    public static final void setPen() {
+        final Pen p = pens[curPen];
+        shapeDrawer = p != null ? new PenShapeDrawer(p)
+                : new SimpleShapeDrawer();
+    }
+
+    public static void main(final String[] args) {
+        setPen();
+        shape = createShape();
+        jFrame = new JFrame();
+        jFrame.add(new JComponent() {
+
+            private static final long serialVersionUID = 2257038010168418793L;
+
+            @Override
+            protected void paintComponent(final Graphics gfx) {
+                final Graphics2D g = (Graphics2D) gfx.create();
+                // g.setColor(Color.BLACK);
+                // g.draw(shape);
+                shapeDrawer.draw(g, shape);
+                g.dispose();
+            }
+
+        });
+        jFrame.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(final MouseEvent e) {
+                ++curPen;
+                if (curPen >= pens.length) {
+                    curPen = 0;
+                }
+                setPen();
+                jFrame.repaint();
+            }
+
+        });
+        jFrame.setSize(new Dimension(800, 600));
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+        jFrame.setVisible(true);
+    }
+
+    private static Shape createShape() {
+        final GeneralPath path = new GeneralPath();
+        path.moveTo(100.0, 100.0);
+        path.lineTo(150.0, 200.0);
+        path.quadTo(250.0, 250.0, 300.0, 150.0);
+        path.curveTo(400.0, 80.0, 450.0, 160.0, 500.0, 400.0);
+        path.moveTo(600.0, 300.0);
+        path.quadTo(650.0, 250.0, 700.0, 300.0);
+        path.quadTo(700.0, 350.0, 650.0, 350.0);
+        path.closePath();
+        path.moveTo(200.0, 350.0);
+        path.quadTo(200.0, 300.0, 250.0, 300.0);
+        path.quadTo(300.0, 300.0, 300.0, 350.0);
+        path.quadTo(300.0, 400.0, 250.0, 400.0);
+        path.quadTo(200.0, 400.0, 200.0, 350.0);
+        return path;
+    }
 
 }
