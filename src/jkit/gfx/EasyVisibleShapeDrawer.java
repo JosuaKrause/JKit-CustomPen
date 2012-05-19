338ebf354707e42cedb590147eb34804bf79661d
Josua Krause <josua.krause@googlemail.com>
2012-05-19 19:51:15 +0200

    merge conflict ;)

--- src/jkit/gfx/EasyVisibleShapeDrawer.java
+++ src/jkit/gfx/EasyVisibleShapeDrawer.java
@@ -10,39 +10,44 @@ import java.awt.geom.Rectangle2D;
 
 public class EasyVisibleShapeDrawer extends AbstractShapeDrawer {
 
-  private final Stroke inner;
+    private final Stroke inner;
 
-  private final Stroke outer;
+    private final Stroke outer;
 
-  private final Color fg;
+    private final Color fg;
 
-  private final Color bg;
+    private final Color bg;
 
-  public EasyVisibleShapeDrawer() {
-    this(Color.WHITE, Color.BLACK, 1, 1);
-  }
+    public EasyVisibleShapeDrawer() {
+        this(Color.WHITE, Color.BLACK, 1, 1);
+    }
 
-  public EasyVisibleShapeDrawer(final Color fg, final Color bg,
-      final double radius, final double outerRadius) {
-    inner = new BasicStroke((float) radius);
-    outer = new BasicStroke((float) (radius + outerRadius + 1));
-    this.fg = fg;
-    this.bg = bg;
-  }
+    public EasyVisibleShapeDrawer(final Color fg, final Color bg,
+            final double radius, final double outerRadius) {
+        inner = new BasicStroke((float) radius);
+        outer = new BasicStroke((float) (radius + outerRadius + 1));
+        this.fg = fg;
+        this.bg = bg;
+    }
 
-  @Override
-  public void draw(final Graphics gfx, final Shape outline) {
-    final Graphics2D g = (Graphics2D) gfx.create();
-    g.setColor(bg);
-    g.fill(outer.createStrokedShape(outline));
-    g.setColor(fg);
-    g.fill(inner.createStrokedShape(outline));
-    g.dispose();
-  }
+    @Override
+    public void setColor(final Color color) {
+        // FIXME
+    }
 
-  @Override
-  public Rectangle2D getBounds(final Shape s) {
-    return outer.createStrokedShape(s).getBounds2D();
-  }
+    @Override
+    public void draw(final Graphics gfx, final Shape outline) {
+        final Graphics2D g = (Graphics2D) gfx.create();
+        g.setColor(bg);
+        g.fill(outer.createStrokedShape(outline));
+        g.setColor(fg);
+        g.fill(inner.createStrokedShape(outline));
+        g.dispose();
+    }
+
+    @Override
+    public Rectangle2D getBounds(final Shape s) {
+        return outer.createStrokedShape(s).getBounds2D();
+    }
 
 }
